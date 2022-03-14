cda2562ea1e8cccf13d4ec4e58875cb7039b1f24
LuizFernandez <a88539@alunos.uminho.pt>
2022-03-14 09:42:56 +0000

    Guião_2 Completo

--- Guiao_2/code/main.cpp
+++ Guiao_2/code/main.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -5,6 +7,9 @@
 #endif
 
 #include <math.h>
+#include <stdio.h>
+
+float posx = 0, posz = 0, angle = 0, scalex = 1, scaley = 1, scalez = 1;
 
 void changeSize(int w, int h) {
 
@@ -31,7 +36,6 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-
 void renderScene(void) {
 
 	// clear buffers
@@ -43,11 +47,65 @@ void renderScene(void) {
 		      0.0,0.0,0.0,
 			  0.0f,1.0f,0.0f);
 
+	//Sistemas de Coordenadas
+	glBegin(GL_LINES);
+		// X axis in red
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(-100.0f, 0.0f, 0.0f);
+		glVertex3f(100.0f, 0.0f, 0.0f);
+		// Y axis in Green
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(0.0f, -100.0f, 0.0f);
+		glVertex3f(0.0f, 100.0f, 0.0f);
+		// Z axis in Blue
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(0.0f, 0.0f, -100.0f);
+		glVertex3f(0.0f, 0.0f, 100.0f);
+	glEnd();
+
 // put the geometric transformations here
 
+	glTranslatef(posx,0.0,posz);
+	glRotatef(angle, 0.0, 1.0, 0.0);
+	glScalef(scalex, scaley, scalez);
 
 // put drawing instructions here
 
+	glBegin(GL_TRIANGLES);
+
+		//Base
+		glColor3ub(75,0,130);
+		glVertex3f(-0.5,0.0,-0.5);
+		glVertex3f(0.5,0.0,-0.5);
+		glVertex3f(0.5,0.0,0.5);
+		
+		glColor3ub(0,0,255);
+		glVertex3f(0.5,0.0,0.5);
+		glVertex3f(-0.5,0.0,0.5);
+		glVertex3f(-0.5,0.0,-0.5);
+
+		//Lados
+		glColor3ub(0,128,0);
+		glVertex3f(-0.5,0.0,-0.5);
+		glVertex3f(0.0,1,0.0);
+		glVertex3f(0.5,0,-0.5);
+
+		glColor3ub(255,255,0);
+		glVertex3f(0.5,0,-0.5);
+		glVertex3f(0.0,1,0.0);
+		glVertex3f(0.5,0,0.5);
+
+		glColor3ub(0,128,0);
+		glVertex3f(0.5,0,0.5);
+		glVertex3f(0.0,1,0.0);
+		glVertex3f(-0.5,0,0.5);
+
+		glColor3ub(255,255,0);
+		glVertex3f(-0.5,0,0.5);
+		glVertex3f(0.0,1,0.0);
+		glVertex3f(-0.5,0,-0.5);
+
+	glEnd();
 
 	// End of frame
 	glutSwapBuffers();
@@ -57,10 +115,51 @@ void renderScene(void) {
 
 // write function to process keyboard events
 
+void keyBoard(unsigned char key, int x, int y){
+	switch (key) {
+	case 'w':
+		posx -= 0.1;
+		break;
+	case 'd':
+		posz -= 0.1;
+		break;
+	case 'a':
+		posz += 0.1;
+		break;
+	case 's':
+		posx += 0.1;
+		break;
+	case 'e':
+		angle -= 10;
+		break;
+	case 'r':
+		angle += 10;
+		break;
+	default:
+		break;
+	}
 
+	glutPostRedisplay();
+}
 
+void keyBoardSpecial(int key_code, int x, int y){
+	switch (key_code) {
+		case GLUT_KEY_UP:
+			scalex++;
+			scaley++;
+			scalez++;
+			break;
+		case GLUT_KEY_DOWN:
+			scalex--;
+			scaley--;
+			scalez--;
+			break;
+		default:
+			break;
+	}
 
-
+	glutPostRedisplay();
+}
 
 int main(int argc, char **argv) {
 
@@ -78,7 +177,8 @@ int main(int argc, char **argv) {
 	
 // put here the registration of the keyboard callbacks
 
-
+	glutKeyboardFunc(keyBoard);
+	glutSpecialFunc(keyBoardSpecial);
 
 //  OpenGL settings
 	glEnable(GL_DEPTH_TEST);
